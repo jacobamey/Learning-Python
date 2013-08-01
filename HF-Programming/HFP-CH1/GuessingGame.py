6a33cdb1009c60e7a097efdb1dfd37065a0cbc14
Jacob Amey <jacobamey@gmail.com>
2013-07-31 22:50:24 -0400

    updates CHP1

--- HF-Programming/HFP-CH1/GuessingGame.py
+++ HF-Programming/HFP-CH1/GuessingGame.py
@@ -1,16 +1,20 @@
 __author__ = 'Jacob Amey'
 
+from random import randint
+
+secret = randint(1, 10)
+
 print("Welcome!")
 
 guess = 0
 
-while guess != 5:
+while guess != secret:
     g = input("Guess the number: ")
     guess = int(g)
-    if guess == 5:
+    if guess == secret:
         print("You win!")
     else:
-        if guess > 5:
+        if guess > secret:
             print("Too High")
         else:
             print("Too low")
