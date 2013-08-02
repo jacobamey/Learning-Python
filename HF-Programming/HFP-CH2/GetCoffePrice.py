e06a3a09937f030e77e39e09df6886d322b932c1
Jacob Amey <jacobamey@gmail.com>
2013-08-01 21:42:02 -0400

    updates CHP2

--- HF-Programming/HFP-CH2/GetCoffePrice.py
+++ HF-Programming/HFP-CH2/GetCoffePrice.py
@@ -1,9 +1,11 @@
 import urllib.request
+import time
 
 price = 99.99
 
 while price > 4.74:
 
+	time.sleep(900)
 	page = urllib.request.urlopen("http://beans-r-us.biz/prices-loyalty.html")
 	text = page.read().decode("utf8")
 
