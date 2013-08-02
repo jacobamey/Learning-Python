8b1f91188d109d13131918debde66e628f65add4
Jacob Amey <jacobamey@gmail.com>
2013-08-01 20:56:39 -0400

    updates CHP2 - fixed bean bug!

--- HF-Programming/HFP-CH2/GetCoffePrice.py
+++ HF-Programming/HFP-CH2/GetCoffePrice.py
@@ -4,6 +4,12 @@ import urllib.request
 
 page = urllib.request.urlopen("http://beans-r-us.biz/prices-loyalty.html")
 text = page.read().decode("utf8")
-price = text[234:238]
+
+where = text.find(">$")
+
+start_of_price = where + 2
+end_of_price = start_of_price + 4
+
+price = text[start_of_price:end_of_price]
 
 print(price)
