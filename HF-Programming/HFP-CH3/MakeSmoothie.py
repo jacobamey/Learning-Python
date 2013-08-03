3ba84f759bec670cad3ca2880a663d560039af1e
Jacob Amey <jacobamey@gmail.com>
2013-08-02 21:07:50 -0400

    updates CHP3

---
+++ HF-Programming/HFP-CH3/MakeSmoothie.py
@@ -0,0 +1,14 @@
+def make_smoothie():
+	juice = input("What juice would you like? ")
+	fruit = input("OK - and how about the fruit? ")
+	print("Thanks. Let's go!")
+	print("Crushing the ice...")
+	print("Blending the " + fruit)
+	print("Now adding in the " + juice + " juice")
+	print("Finished! There's your " + fruit + " and " + juice + " smoothie!")
+
+print("Welcome to smoothie-matic 2.0")
+another = "Y"
+while another == "Y":
+	make_smoothie()
+	another = input("How about another(Y/N)? ")
