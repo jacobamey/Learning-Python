ce2d7ac8292e3cde6c4a8dff07af95c2a2434516
Jacob Amey <jacobamey@gmail.com>
2013-08-05 21:41:53 -0400

    update HFP CH4 - 3rd commit

--- HF-Programming/HFP-CH4/highScore.py
+++ HF-Programming/HFP-CH4/highScore.py
@@ -1,9 +1,10 @@
-highest_score = 0
+scores = []
 result_f = open("results.txt")
 for line in result_f:
     (name, score) = line.split()
-    if float(score) > highest_score:
-        highest_score = float(score)
+    scores.append(float(score))
 result_f.close()
-print("The Highest score was: ")
-print(highest_score)
+print("The top score was: ")
+print(scores[0])
+print(scores[1])
+print(scores[2])
