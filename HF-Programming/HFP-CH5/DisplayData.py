ed5c4325ee8001d802c93b1825067d498220a3d8
Jacob Amey <jacobamey@gmail.com>
2013-08-06 22:51:57 -0400

    update HFP CH5 - 4th commit

--- HF-Programming/HFP-CH5/DisplayData.py
+++ HF-Programming/HFP-CH5/DisplayData.py
@@ -1,20 +1,32 @@
-def find_details(id2find):
-    surfers_f = open("surfing_data.csv")
-    for each_line in surfers_f:
-        s = {}
-        (s['id'], s['name'], s['country'], s['average'], s['board'], s['age']) = each_line.split(";")
-        if id2find == int(s['id']):
-            surfers_f.close()
-            return(s)
-        surfers_f.close()
-        return({})
+import sqlite3
 
-lookup_id = int(input("Enter the id of the surfer: "))
-surfer = find_details(lookup_id)
-if surfer:
-    print("ID:          " + surfer['id'])
-    print("Name:        " + surfer['name'])
-    print("Country:     " + surfer['country'])
-    print("Average:     " + surfer['average'])
-    print("Board type:  " + surfer['board'])
-    print("Age:         " + surfer['age'])
+
+def find_details(id2find): 
+    db = sqlite3.connect("surfersDB.sdb") 
+    db.row_factory = sqlite3.Row 
+    cursor = db.cursor() 
+    cursor.execute("select * from surfers") 
+    rows = cursor.fetchall() 
+    for row in rows: 
+        if row['id'] == id2find: 
+            s = {}
+            s['id'] = str(row['id'])
+            s['name'] = row['name']
+            s['country'] = row['country']
+            s['average'] = str(row['average'])
+            s['board'] = row['board']
+            s['age'] = str(row['age'])
+            cursor.close() 
+            return(s) 
+    cursor.close() 
+    return({})
+
+lookup_id = int(input("Enter the id of the surfer: ")) 
+surfer = find_details(lookup_id) 
+if surfer: 
+    print("ID:         " + surfer['id']) 
+    print("Name:       " + surfer['name']) 
+    print("Country:    " + surfer['country']) 
+    print("Average:    " + surfer['average']) 
+    print("Board type: " + surfer['board']) 
+    print("Age:        " + surfer['age'])
