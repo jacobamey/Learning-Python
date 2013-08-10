eaf338c7ab7263d2cdff1720f524be4392df0c7b
Jacob Amey <jacobamey@gmail.com>
2013-08-09 22:14:39 -0400

    CHP 6 - Update

--- HF-Programming/HFP-CH6/CoffeePos.py
+++ HF-Programming/HFP-CH6/CoffeePos.py
@@ -1,7 +1,8 @@
 from transactions import *
 
+
 items = ["DONUT", "LATTE", "FILTER", "MUFFIN"]
-prices = [1.50, 2.0, 1.80, 1.20]
+prices = [1.50, 2.20, 1.80, 1.20]
 running = True
 
 while running:
@@ -15,4 +16,7 @@ while running:
         running = False
     else:
         credit_card = input("Credit card number: ")
-        save_transaction(prices[choice - 1], credit_card, items[choice - 1])
+        price = promotion.discount(prices[choice - 1])
+        if input("Starbuzz card? ") == "Y":
+            price = starbuzz.discount(price)
+        save_transaction(price, credit_card, items[choice - 1])
--- HF-Programming/HFP-CH6/transactions.py
+++ HF-Programming/HFP-CH6/transactions.py
@@ -1,4 +1,4 @@
 def save_transaction(price, credit_card, description):
     file = open("transactions.txt", "a")
-    file.write("%s%07d%s\n" % (price * 100, credit_card, description))
+    file.write("%s%07d%s\n" % (credit_card, price * 100, description))
     file.close()
