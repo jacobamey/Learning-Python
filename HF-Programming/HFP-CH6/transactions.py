ec00e1019f75cd203306de3bb769604384382cca
Jacob Amey <jacobamey@gmail.com>
2013-08-10 19:23:06 -0400

    CHP 6 - Update

--- HF-Programming/HFP-CH6/transactions.py
+++ HF-Programming/HFP-CH6/transactions.py
@@ -1,4 +1,5 @@
-def save_transaction(price, credit_card, description): 
+#!/usr/bin/env python
+def save_transaction(price, credit_card, description):
     file = open("transactions.txt", "a")
     file.write("%s%07d%s\n" % (credit_card, price * 100, description)) 
     file.close()
