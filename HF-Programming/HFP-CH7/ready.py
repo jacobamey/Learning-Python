d1ee339b7902eddb98039c9224241dd3d4dd4dc2
Jacob Amey <jacobamey@gmail.com>
2013-08-11 01:38:04 -0400

    CHP 7 - Update, pygame does not work. abandon sound making.

--- HF-Programming/HFP-CH7/ready.py
+++ HF-Programming/HFP-CH7/ready.py
@@ -1,22 +1,9 @@
-#!/usr/bin/env python3
-import os
-import random
-from glob import glob
+import wave
 
-import pyglet
 
 # load sounds
-paths = glob(os.path.expanduser('/home/jake/Programming/origin/Learning-Python/HF-Programming.HFP-CH7/*.wav'))
-sounds = [pyglet.media.load(p, streaming=False) for p in paths]
+paths = wave.Wave_read("/home/jake/Programming/origin/Learning-Python/HF-Programming.HFP-CH7/*.wav")
+sounds = wave.Wave_write()
 
-# play them in parallel
-for sound in sounds:
-    player = sound.play()
-    player.volume = random.random()
-    player.push_handlers(on_eos=lambda: print('done playing the sound'))
 
-# exit in `delay` seconds
-pyglet.clock.schedule_once(lambda dt: pyglet.app.exit(), delay=10)
-# run event loop
-pyglet.app.run()
 
