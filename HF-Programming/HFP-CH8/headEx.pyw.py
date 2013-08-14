46a531fa9f208687bc1dd7fab154e910a09015eb
Jacob Amey <jacobamey@gmail.com>
2013-08-13 23:43:42 -0400

    CHP 8 - First run, page 269.

--- HF-Programming/HFP-CH8/headEx.pyw.py
+++ HF-Programming/HFP-CH8/headEx.pyw.py
@@ -1,5 +1,18 @@
 from tkinter import *
 
+
+def save_data():
+    fileD = open("deliveries.txt", "a")
+    fileD.write("Depot:\n")
+    fileD.write("%s\n" % depot.get())
+    fileD.write("Description:\n")
+    fileD.write("%s\n" % description.get())
+    fileD.write("Address:\n")
+    fileD.write("%s\n" % address.get("1.0", END))
+    depot.delete(0, END)
+    description.delete(0, END)
+    address.delete(1.0, END)
+
 app = Tk()
 app.title('Head-Ex deliveries')
 
@@ -9,6 +22,7 @@ depot.pack()
 
 Label(app, text="Description:").pack()
 description = Entry(app)
+description.pack()
 
 Label(app, text="Address:").pack()
 address = Text(app)
