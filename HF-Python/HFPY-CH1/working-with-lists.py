b8f78ca903266af036911be57499b03baa36919d
Jacob Amey <jacobamey@gmail.com>
2013-08-22 00:40:48 -0400

    CH 1 - changed deep isinstance() utilizing if and for loops.

--- HF-Python/HFPY-CH1/working-with-lists.py
+++ HF-Python/HFPY-CH1/working-with-lists.py
@@ -5,8 +5,12 @@ movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
 print(movies)
 
 for each_item in movies:
-    if isinstance(each_item, list):
-        for nested_item in each_item:
-            print(nested_item)
-    else:
-        print(each_item)
+        if isinstance(each_item, list):
+            for nested_item in each_item:
+                if isinstance(nested_item, list):
+                    for deeper_item in nested_item:
+                            print(deeper_item)
+            else:
+                print(nested_item)
+        else:
+            print(each_item)
