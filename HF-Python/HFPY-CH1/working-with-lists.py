d5491d071660d0f136713ff03aff4ad5425e54c9
Jacob Amey <jacobamey@gmail.com>
2013-08-21 22:29:10 -0400

    CH 1 - testing append()

--- HF-Python/HFPY-CH1/working-with-lists.py
+++ HF-Python/HFPY-CH1/working-with-lists.py
@@ -1,7 +1,9 @@
 cast = ["Cleese", 'pain', 'Jones', "Idle"]
 
+cast.append("Gilliam")
+
 # Prints the list as is.
 #print(cast)
 
 # Prints the count of items in the list.
-print(len(cast))
+print(cast)
