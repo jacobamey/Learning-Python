1732cb4603bd265f8a80f4ed51836abc704a0d85
Jacob Amey <jacobamey@gmail.com>
2013-08-22 00:43:20 -0400

    CH 1 - removed an extra print(movies statment at the top.)

--- HF-Python/HFPY-CH1/working-with-lists.py
+++ HF-Python/HFPY-CH1/working-with-lists.py
@@ -2,7 +2,6 @@ movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
                 ["Graham Chapman", ["Michael Palin", "John Cleese",
                         "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
 
-print(movies)
 
 for each_item in movies:
     if isinstance(each_item, list):
