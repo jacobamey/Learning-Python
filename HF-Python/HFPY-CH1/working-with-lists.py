4c90d9a41bd0a699de6912775ae4bdac61f8e8c6
Jacob Amey <jacobamey@gmail.com>
2013-08-22 00:39:40 -0400

    CH 1 - changed for loop.

--- HF-Python/HFPY-CH1/working-with-lists.py
+++ HF-Python/HFPY-CH1/working-with-lists.py
@@ -5,4 +5,8 @@ movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
 print(movies)
 
 for each_item in movies:
-    print(each_item)
+    if isinstance(each_item, list):
+        for nested_item in each_item:
+            print(nested_item)
+    else:
+        print(each_item)
