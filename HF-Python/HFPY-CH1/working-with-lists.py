be78b28f3ed1a1def28c63e972768d4372342c5c
Jacob Amey <jacobamey@gmail.com>
2013-08-22 00:44:18 -0400

    CH 1 - final product utilizing a recursive function.

--- HF-Python/HFPY-CH1/working-with-lists.py
+++ HF-Python/HFPY-CH1/working-with-lists.py
@@ -3,17 +3,12 @@ movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
                         "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
 
 
-for each_item in movies:
-    if isinstance(each_item, list):
-        for nested_item in each_item:
-            if isinstance(nested_item, list):
-                for deeper_item in nested_item:
-                    if isinstance(deeper_item, list):
-                                                for deepest_item in deeper_item:
-                                                    print(deepest_item)
-                    else:
-                        print(deeper_item)
-            else:
-                print(nested_item)
-    else:
-        print(each_item)
+def print_lol(a_list):
+    for each_item in a_list:
+        if isinstance(each_item, list):
+            print_lol(each_item)
+        else:
+            print(each_item)
+
+
+print_lol(movies)
