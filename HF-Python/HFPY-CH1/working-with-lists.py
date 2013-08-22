e68ef86686ab2d4b728b01f2d3e03fc451efd2ab
Jacob Amey <jacobamey@gmail.com>
2013-08-22 00:33:32 -0400

    CH 1 - first commit, actual program.

--- HF-Python/HFPY-CH1/working-with-lists.py
+++ HF-Python/HFPY-CH1/working-with-lists.py
@@ -1,5 +1,8 @@
-cast = ["Cleese", 'pain', 'Jones', "Idle"]
-cast.extend(["Gilliam", "Chapman"])
-cast.remove("Chapman")
-cast.insert(0, "Chapman")
-print(cast)
+movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
+                ["Graham Chapman", ["Michael Palin", "John Cleese",
+                        "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
+
+print(movies)
+
+for each_item in movies:
+    print(each_item)
