a331635448f0bb16b3e46ed2a3c180921318d335
Jacob Amey <jacobamey@gmail.com>
2013-08-22 00:42:42 -0400

    CH 1 - crazy for loop and if statement! mind blown = official.

--- HF-Python/HFPY-CH1/working-with-lists.py
+++ HF-Python/HFPY-CH1/working-with-lists.py
@@ -5,12 +5,16 @@ movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
 print(movies)
 
 for each_item in movies:
-        if isinstance(each_item, list):
-            for nested_item in each_item:
-                if isinstance(nested_item, list):
-                    for deeper_item in nested_item:
-                            print(deeper_item)
+    if isinstance(each_item, list):
+        for nested_item in each_item:
+            if isinstance(nested_item, list):
+                for deeper_item in nested_item:
+                    if isinstance(deeper_item, list):
+                                                for deepest_item in deeper_item:
+                                                    print(deepest_item)
+                    else:
+                        print(deeper_item)
             else:
                 print(nested_item)
-        else:
-            print(each_item)
+    else:
+        print(each_item)
