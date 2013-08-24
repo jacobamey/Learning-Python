d572dbeaeff3d7f9ea496a6db7a2dfa30690215e
Jacob Amey <jacobamey@gmail.com>
2013-08-23 23:02:15 -0400

    CH 2 - commented (heavily) nester.py

--- HF-Python/HFPY-CH2/nester.py
+++ HF-Python/HFPY-CH2/nester.py
@@ -1,4 +1,11 @@
+"""This is the "nester.py" module, and it provides one function called
+print_lol() which prints lists that may or may not include nested lists"""
+
+
 def print_lol(a_list):
+    """This function takes a positional argument called "the_list", which is any
+     Python list (of, possibly, nested lists). Each data item in the provided list
+     is (recursively) printed to the screen on its own line"""
     for each_item in a_list:
         if isinstance(each_item, list):
             print_lol(each_item)
