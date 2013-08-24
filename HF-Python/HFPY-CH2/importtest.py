204de347510ac7761e40fed33f57f8675a3216b3
Jacob Amey <jacobamey@gmail.com>
2013-08-24 02:00:34 -0400

    CH 2 - updated recurs_print.py to 1.1.0, uploaded to PyPI

--- HF-Python/HFPY-CH2/importtest.py
+++ HF-Python/HFPY-CH2/importtest.py
@@ -1,5 +1,10 @@
 import recurs_print
 
-cast = ['Palin', 'Clease', 'Idle', 'Jones', 'Gilliam', 'Chapman']
+#cast = ['Palin', 'Clease', 'Idle', 'Jones', 'Gilliam', 'Chapman']
 
-recurs_print.list_print(cast)
+movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
+                ["Graham Chapman", ["Michael Palin", "John Cleese",
+                        "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
+
+
+recurs_print.list_print(movies, True, 1)
--- HF-Python/HFPY-CH2/recurs_print.py
+++ HF-Python/HFPY-CH2/recurs_print.py
@@ -2,12 +2,18 @@
 list_print() which prints lists that may or may not include nested lists"""
 
 
-def list_print(a_list):
+def list_print(a_list, indent=False, level=0):
     """This function takes a positional argument called "a_list", which is any
      Python list (of, possibly, nested lists). Each data item in the provided list
-     is (recursively) printed to the screen on its own line"""
+     is (recursively) printed to the screen on its own line
+     A second argument called "level" is used to insert tab-stops when a nested list is encountered.
+      the 3rd argument indent which can be True or False allows the user to ignore or use the level
+      (Indent feature)"""
     for each_item in a_list:
         if isinstance(each_item, list):
-            list_print(each_item)
+            list_print(each_item, indent, level+1)
         else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t", end='')
             print(each_item)
