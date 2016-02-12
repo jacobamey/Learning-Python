adddf51e73982ced65cd0e7759c966403fa816ff
Jacob Amey <jacobamey@gmail.com>
2016-02-11 20:41:43 -0500

    Revisited old code, picking project back up.

--- HF-Python/HFPY-CH1/working-with-lists.py
+++ HF-Python/HFPY-CH1/working-with-lists.py
@@ -1,9 +1,10 @@
+# coding=utf-8
 movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
-                ["Graham Chapman", ["Michael Palin", "John Cleese",
-                        "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
+          ["Graham Chapman", ["Michael Palin", "John Cleese", "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
 
 
 def print_lol(a_list):
+
     for each_item in a_list:
         if isinstance(each_item, list):
             print_lol(each_item)
