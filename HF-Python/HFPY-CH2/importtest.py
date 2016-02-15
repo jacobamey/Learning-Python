80d8b01600115bbf0f3e9a595f372dc49ce5affb
Jacob Amey <jacobamey@gmail.com>
2016-02-14 20:03:53 -0500

    Revisited old code, picking project back up.

--- HF-Python/HFPY-CH2/importtest.py
+++ HF-Python/HFPY-CH2/importtest.py
@@ -1,10 +1,11 @@
+# coding=utf-8
 import recurs_print
 
 # cast = ['Palin', 'Clease', 'Idle', 'Jones', 'Gilliam', 'Chapman']
 
 movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
-                ["Graham Chapman", ["Michael Palin", "John Cleese",
-                        "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
+          ["Graham Chapman",
+           ["Michael Palin", "John Cleese", "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
 
 
 recurs_print.list_print(movies, True, 1)
